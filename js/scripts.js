b45e2790e4233b46222a4f329635b29d8a81ec64
meglybbert <meglybbert@eagles.ewu.edu>
2017-06-15 21:15:24 -0700

    Added sortable successfully; broke canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,64 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
-console.log('Hello, World!');
+// CONFIGURE & "Handshake"
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: 'keyDmRaxoESviMOwB'
+});
+var base = Airtable.base('appM52ABdf4HMwFMh');
+// Check-Check
+console.log(base);
 
-const canvas = document.querySelector('#draw');
+// Get Records
+base('felines').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    
+    if (err) { console.error(err); return; }
+
+    records.forEach(function(record) {
+       
+      // Check-Check 
+      // console.log('Retrieved', record.get('Name'));
+      // console.log( record.fields.Avatar[0].url ); 
+
+      // Display Data
+      showKitties(record)
+
+    });
+});
+
+// Template Literal
+var showKitties = function(record) {
+
+  var template = 
+  `
+    <section class="ui raised card">
+        <div class="image">
+           <canvas class="draw" style="background-image:url('${record.fields.Image[0].url}'); background-size: contain; background-repeat: no-repeat; background-position: center;"></canvas>
+        </div>
+       <div class="content">
+           <h3>${record.fields.Name}</h3>
+       </div>
+    </section>
+  `;
+
+  // Display Collected Data
+  $('#kitties').append(template);
+}
+
+//Draw Stuff
+const canvas = document.querySelector('.draw');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = '#3b3b3b';
+canvas.width = ($(".image").innerWidth());
+canvas.height = ($(".image").innerHeight());
+ctx.strokeStyle = 'aquamarine';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 40;
+ctx.lineWidth = 10;
 //ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
@@ -55,3 +103,5 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
+
+
